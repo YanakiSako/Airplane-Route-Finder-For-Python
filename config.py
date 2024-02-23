686b22c8b5c233a34f21ed2bb76bec39e1cfeed5
YanakiSako <161080754+YanakiSako@users.noreply.github.com>
2024-02-24 04:35:25 +0800

    First submit
    This project first submit

---
+++ config.py
@@ -0,0 +1,8 @@
+LOCAL_ASDATA_PATH = "PLEASE CHECK CONFIG.PY FILES!"
+LISTEN_PORT = 11451
+METAR_UPDATE_MINUTE = 15
+YourBingMapsKey = "Use Your Own Key"
+BackstageKey = "set your self"
+SET_NAVDAT_PATH = "navidata.map" #NAVDATA
+SET_APDAT_PATH = "apdat.air" #APDAT
+NAVDAT_CYCLE = "ovo"
